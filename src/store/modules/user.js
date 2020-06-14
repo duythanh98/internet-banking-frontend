6368f5c4a265ec3ee69ca1beff031a2c613888d1
Võ Duy Thanh <duythanh180@gmail.com>
2020-06-14 15:10:52 +0700

    - Add reCaptcha v3 - Edit logout function

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,4 @@
-import { logout } from '@/api/user';
+// import { logout } from '@/api/user';
 import { getToken, setToken, removeToken } from '@/utils/auth';
 import router, { resetRouter } from '@/router';
 import LoginApi from '@/api/prod/login.api';
@@ -48,6 +48,7 @@ const actions = {
     }
 
     const result = res.result();
+    console.log(result);
 
     commit('SET_TOKEN', result.access_token);
     commit('SET_REFRESH_TOKEN', result.refresh_token);
@@ -87,22 +88,37 @@ const actions = {
   },
 
   // user logout
+  // logout({ commit, state, dispatch }) {
+  //   return new Promise((resolve, reject) => {
+  //     logout(state.token).then(() => {
+  //       commit('SET_TOKEN', '');
+  //       commit('SET_ROLES', []);
+  //       removeToken();
+  //       resetRouter();
+
+  //       // reset visited views and cached views
+  //       // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
+  //       dispatch('tagsView/delAllViews', null, { root: true });
+
+  //       resolve();
+  //     }).catch(error => {
+  //       reject(error);
+  //     });
+  //   });
+  // },
+
   logout({ commit, state, dispatch }) {
     return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        commit('SET_TOKEN', '');
-        commit('SET_ROLES', []);
-        removeToken();
-        resetRouter();
+      commit('SET_TOKEN', '');
+      commit('SET_ROLES', []);
+      removeToken();
+      resetRouter();
 
-        // reset visited views and cached views
-        // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
-        dispatch('tagsView/delAllViews', null, { root: true });
+      // reset visited views and cached views
+      // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
+      dispatch('tagsView/delAllViews', null, { root: true });
 
-        resolve();
-      }).catch(error => {
-        reject(error);
-      });
+      resolve();
     });
   },
 
